0805b6f5edfb0bcbfdf28d496ff740da9452f2ae
ahmadfauzy <ahmadfauzy121995@gmail.com>
2016-09-04 15:12:48 +0800

    pembaharuan views fauzy

--- views/view_homepage.php
+++
@@ -1,6 +0,0 @@
-<!--JANGAN DIHAPUS TAG DIV DIBAWAH-->
-</div><!--/.fluid-container-->
-<!-- end: Content -->
-</div><!--/#content.span10-->
-</div><!--/fluid-row-->
-<!--JANGAN DIHAPUS TAG DIV DIATAS-->
